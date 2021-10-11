b0865b8a08f1b845fbafd00842590db354fa975d
Souleymane DIALLO <s.diallo@macway.com>
2021-10-11 16:24:38 +0200

    added auth

---
+++ src/aws-exports.js
@@ -0,0 +1,13 @@
+const awsmobile =  {
+    "aws_project_region": "eu-central-1",
+    "aws_cognito_identity_pool_id": "eu-central-1:d922c508-2d31-4acc-9333-82bcee3d9ce3",
+    "aws_cognito_region": "eu-central-1",
+    "aws_user_pools_id": "eu-central-1_2nh2zNHTc",
+    "aws_user_pools_web_client_id": "4gbuse7iee4enk7e27pekcg3t2",
+    "aws_appsync_graphqlEndpoint": "https://ig4gywcicbambdxqyfzoulekoi.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-central-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
+};
+
+
+export default awsmobile;
